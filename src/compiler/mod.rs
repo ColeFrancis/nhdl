b3a6f3980f537ff5975004a4a45c5cc17f347c99
ColeFrancis <155692856+ColeFrancis@users.noreply.github.com>
2026-07-09 23:52:26 -0600

    Add copyright and license comments
    Add copyright notice and license information to mod.rs

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -1,3 +1,14 @@
+// Copyright 2026 Cole Francis
+//
+// Licensed under the Apache License, Version 2.0.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND.
+
 pub mod lexer;
 pub mod token;
 pub mod parser;
