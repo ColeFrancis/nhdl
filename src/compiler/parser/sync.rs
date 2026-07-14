02a81aeb8bd674d9f5bf4b61809a7d30817d00fa
Cole <coleshanefrancis@gmail.com>
2026-07-13 21:02:10 -0600

    Think I finished synchronization for error recovery. Yet to test

--- src/compiler/parser/sync.rs
+++ src/compiler/parser/sync.rs
@@ -49,19 +49,198 @@ impl<'a> Parser<'a> {
         }
     }
 
-    fn sync_item(&mut self) {
+    /*
+    ???
+    let x = 1;
 
+    let x = 5;
+    ???
+    let y = 6;
+
+    foo bar baz;
+    ent Bool = Bool;
+
+    let x = 5
+    let y = 10;
+
+    let = 5;
+    let x = 6;
+
+    let x 5;
+    let y = 2;
+
+    ent_t Foo =
+    rel_t Add:() -> Int = 0;
+
+    rel_t Add(a : Int) -> Int = 0;
+    let x = 1;
+
+    rel_t Add:(a Int) -> Int = 0;
+    ent_t X = Bool;
+
+    rel_t Add(a : Int) -> Int = {
+        let x = 1;
+        x
+    };
+    ent_t X = Bool;
+
+    rel_t Add:(a: Int )-> Int = {
+        let x = 1;
+        x
+    }
+    ent_t X = Bool;
+
+    rel_t Foo:()-> Int = {
+        let x=1;
+        x
+    let y = 5;
+
+    rel_t Foo:()->Int =
+        let x = 1;
+        x
+    }
+    let y = 5;
+    
+    let = ;
+    ent_t ;
+    rel_t ;
+    let x = 1;
+
+    ?????
+
+
+    */
+    fn sync_item(&mut self) {
+        let mut depth = 0;
+        loop {
+            match self.peek().kind {
+                TokenKind::Eof => break,
+
+                TokenKind::LBrace => depth += 1;
+                TokenKind::RBrace => {
+                    if depth > 0 {
+                        depth -= 1;
+                    }
+                    else {
+                        break;
+                    }
+                }
+                
+                TokenKind::Ent_t => break,
+                TokenKind::Rel_t => break,
+                TokenKind::NetToken => break,
+                TokenKind::Let if depth == 0 => break,
+
+                TokenKind::Semicolon if depth == 0 => {
+                    self.next();
+                    break;
+                }
+
+                _ => (),
+            }
+
+            self.next();
+        }
     }
 
     fn sync_inst(&mut self) {
+        let mut depth = 0;
 
+        loop {
+            match self.peek().kind {
+                TokenKind::Eof => break,
+
+                TokenKind::LBrace => depth += 1,
+                TokenKind::RBrace => {
+                    if depth > 0 {
+                        depth -= 1;
+                    }
+                    else {
+                        break;
+                    }
+                }
+
+                TokenKind::Input => break,
+                TokenKind::Output => break,
+                TokenKind::Init => break,
+
+                TokenKind::Ident => match self.peek_n(2) {
+                    TokenKind::Connect => break,
+                    TokenKind::LBrace => break,
+                }
+
+                TokenKind::Semicolon if depth == 0 {
+                    self.next();
+                    break;
+                }
+
+                _ => (),
+            }
+
+            self.next();
+        }
     }
 
     fn sync_statement(&mut self) {
+        let mut depth = 0;
 
+        loop {
+            match self.peek().kind {
+                Eof => break,
+
+                TokenKind::LBrace => depth += 1,
+                TokenKind::RBrace => {
+                    if depth > 0 {
+                        depth -= 1;
+                    }
+                    else {
+                        break;
+                    }
+                }
+
+                TokenKind::Let if depth == 0 => break,
+
+                TokenKind::Semicolon if depth == 0 {
+                    self.next();
+                    break;
+                }
+
+                _ => (),
+            }
+
+            self.next();
+        }
     }
 
     fn sync_expr(&mut self) {
+        let mut depth = 0;
+        loop {
+            match self.peek().kind {
+                TokenKind::Eof => break,
 
+                TokenKind::LBrace => depth += 1;
+                TokenKind::RBrace => {
+                    if depth > 0 {
+                        depth -= 1;
+                    }
+                    else {
+                        break;
+                    }
+                }
+        
+                TokenKind::Semicolon if depth == 0 => break,
+
+                _ => (),
+            }
+
+            self.next();
+        }
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    
 }
