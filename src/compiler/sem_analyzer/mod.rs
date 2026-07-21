d26b58d9585c0cc93bb8f1ad64b1a662a7d5bf17
ColeFrancis <155692856+ColeFrancis@users.noreply.github.com>
2026-07-21 13:54:02 -0600

    Update mod.rs

--- src/compiler/sem_analyzer/mod.rs
+++ src/compiler/sem_analyzer/mod.rs
@@ -11,3 +11,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+pub mod ann_ast;
+mod core;
+
+use crate::compiler::diagnostics::Diagnostics;
+
+pub struct SemAnalyzer<'a> {
+    diagnostics: &'a mut Diagnostics,
+}
